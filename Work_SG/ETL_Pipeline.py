f1066af7b2a83bd3782f9c863cbfa6f6235562de
Oh-SulGi <dhtmfrl0615@naver.com>
2023-08-08 18:21:05 +0900

    1.1 feat : 행정동 리스트 업데이트

--- Work_SG/ETL_Pipeline.py
+++ Work_SG/ETL_Pipeline.py
@@ -2,7 +2,20 @@ def extract():
     '''
     api 데이터 호출 함수
     '''
-    pass
+    import os
+    import requests
+    import xmltodict
+    from dotenv import load_dotenv
+    load_dotenv()
+
+    service_key = os.environ.get('servicekey')
+    url = 'http://apis.data.go.kr/1611000/nsdi/IndvdLandPriceService/attr/getIndvdLandPriceAttr'
+    params ={'serviceKey' : service_key, 'pnu' : '1111017700102110000', 'stdrYear' : '2015', 'format' : 'xml', 'numOfRows' : '10', 'pageNo' : '1' }
+
+    response = requests.get(url, params=params)
+    html = response.text
+    html_dict = xmltodict.parse(html)
+    return html_dict
 
 def transform():
     '''
@@ -40,3 +53,18 @@ def load():
     AWS S3에 적재하는 함수
     '''
     pass
+
+def save_local(data):
+    '''
+    json 데이터를 로컬에 저장하는 함수
+    '''
+    import json
+    
+    path = 'D:/data/project/test1.json'
+    with open(path, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+file = extract()
+# print(file)
+
+save_local(file)
